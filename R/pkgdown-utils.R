b7ce277a7f796dee19898b5c4787d152cdfeddbf
Anna Krystalli <annakrystalli@googlemail.com>
2024-01-22 17:47:30 +0200

    Add add_website_needs function to include hubStyle pkg in doc builds

---
+++ R/pkgdown-utils.R
@@ -0,0 +1,12 @@
+#' Add website Needs field to DESCRIPTION
+#'
+#' Adds the hubverve hubStyle package as a website need to the DESCRIPTION file.
+#' @export
+#'
+add_website_needs <- function() {
+    proj_desc_field_update(
+        key = "Config/Needs/website",
+        value = "Infectious-Disease-Modeling-Hubs/hubStyle",
+        append = TRUE
+    )
+}
--- R/usethis-utils.R
+++ R/usethis-utils.R
@@ -25,3 +25,29 @@ find_template <- function(template_name, package = "usethis") {
   }
   path
 }
+
+# Update a description field
+# Source: https://github.com/r-lib/usethis/blob/a795b743a49bf18975dfad2cf6c8a3b918015cb8/R/proj-desc.R#L32
+proj_desc_field_update <- function (key, value, overwrite = TRUE, append = FALSE)
+{
+  checkmate::assert_character(key, len = 1L)
+  checkmate::assert_character(value)
+  checkmate::assert_logical(overwrite, len = 1L)
+  checkmate::assert_logical(append, len = 1L)
+
+  desc <- desc::desc(file = usethis::proj_get())
+  old <- desc$get_list(key, default = "")
+  if (all(value %in% old)) {
+    return(invisible())
+  }
+  if (!overwrite && length(old > 0) && any(old != "")) {
+    usethis::ui_stop("{usethis::ui_field(key)} has a different value in DESCRIPTION. \\\n      Use {usethis::ui_code('overwrite = TRUE')} to overwrite.")
+  }
+  usethis::ui_done("Adding {usethis::ui_value(value)} to {usethis::ui_field(key)}")
+  if (append) {
+    value <- union(old, value)
+  }
+  desc$set_list(key, value)
+  desc$write()
+  invisible()
+}
