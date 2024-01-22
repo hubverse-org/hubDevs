3198cf895fbc6935b218dc0f50a6eab217d3acd9
Anna Krystalli <annakrystalli@googlemail.com>
2024-01-22 18:11:54 +0200

    Add use_hubdev_pkgdown

---
+++ R/use_hubdev_pkgdown.R
@@ -0,0 +1,21 @@
+#' Add hubverse _pkgdown.yml
+#'
+#' Adds a customised _pkgdown.yml file to the package root directory configured to
+#' use the hubverse hubStyle package for docs styling.
+#' @export
+use_hubdev_pkgdown <- function() {
+
+  data <- list(
+    organisation = "infectious-disease-modeling-hubs",
+    pkgname = get_pkgname()
+  )
+
+  usethis::use_template(
+    "_pkgdown.yml",
+    "_pkgdown.yml",
+    data = data,
+    ignore = TRUE,
+    open = FALSE,
+    package = "hubDevs"
+  )
+}
