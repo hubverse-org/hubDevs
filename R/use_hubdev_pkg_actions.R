e09959d052902f7f3524410fe2db722e7b334557
Anna Krystalli <annakrystalli@googlemail.com>
2024-01-31 12:13:09 +0200

    Fix lint long line issue

--- R/use_hubdev_pkg_actions.R
+++ R/use_hubdev_pkg_actions.R
@@ -4,8 +4,10 @@
 #' `.github/workflows` directory for:
 #'    - standard R CMD CHECK [(`check-standard`)](
 #' https://github.com/r-lib/actions/blob/v2-branch/examples/check-standard.yaml)
-#'    - test coverage [(`test-coverage`)](https://github.com/r-lib/actions/blob/v2-branch/examples/test-coverage.yaml)
-#'    - linting with `lintr` [(`lint`)](https://github.com/r-lib/actions/blob/v2-branch/examples/lint.yaml)
+#'    - test coverage [(`test-coverage`)](
+#' https://github.com/r-lib/actions/blob/v2-branch/examples/test-coverage.yaml)
+#'    - linting with `lintr` [(`lint`)](
+#' https://github.com/r-lib/actions/blob/v2-branch/examples/lint.yaml)
 #'
 #' @export
 use_hubdev_pkg_actions <- function() {
