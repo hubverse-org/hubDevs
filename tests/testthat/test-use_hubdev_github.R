1bf62d38835ebb82a051974233020a18e61439f3
Anna Krystalli <annakrystalli@googlemail.com>
2026-01-12 11:37:47 +0200

    Add test for use_hubdev_pkgdown_action()
    - Test that pkgdown-netlify-preview.yaml workflow is created
    - Test that standard pkgdown.yaml is removed when remove_std=TRUE
    - Snapshot the Netlify setup instructions to catch accidental changes

--- tests/testthat/test-use_hubdev_github.R
+++ tests/testthat/test-use_hubdev_github.R
@@ -47,3 +47,35 @@ test_that("use_hubdev_pkgdown works", {
     c("url: ~", "template:", "  bootstrap: 5", "")
   )
 })
+
+
+test_that("use_hubdev_pkgdown_action works", {
+  temp_dir <- tempdir()
+  pkg_path <- file.path(temp_dir, "testPkg")
+
+  suppressMessages(create_hubdev_pkg(pkg_path))
+
+  current_wd <- getwd()
+  setwd(pkg_path)
+  on.exit(setwd(current_wd))
+
+  # Add a fake GitHub remote so usethis::use_github_action() can add badges
+  system2("git", c("remote", "add", "origin", "https://github.com/hubverse-org/testPkg.git"))
+
+  # Create a fake standard pkgdown.yaml to test removal
+
+  fs::dir_create(".github/workflows")
+  fs::file_create(".github/workflows/pkgdown.yaml")
+  expect_true(fs::file_exists(".github/workflows/pkgdown.yaml"))
+
+  # Snapshot the full output including Netlify setup instructions
+  expect_snapshot(
+    use_hubdev_pkgdown_action()
+  )
+
+  # Check workflow file was created
+  expect_true(fs::file_exists(".github/workflows/pkgdown-netlify-preview.yaml"))
+
+  # Check standard pkgdown.yaml was removed
+  expect_false(fs::file_exists(".github/workflows/pkgdown.yaml"))
+})
